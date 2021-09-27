d2cc27279e364932150893ffee045881f986709a
gherman <gherman@adp.com>
2021-09-27 10:18:04 -0300

    custom hooks

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,32 +1,17 @@
-import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks'
-import React, { useState } from 'react';
+import React from 'react';
+import { useChatMessage } from './hooks';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
-import { messagesQuery, messageAddedSubscription, addMessageMutation } from './graphql/queries';
 
 const Chat = ({ user }) => {
-    const [messages, setMessage] = useState([]);
-
-    useQuery(messagesQuery, {
-        onCompleted: ({ messages }) => setMessage(messages)
-    });
-  useSubscription(messageAddedSubscription, {
-     onSubscriptionData: ({ subscriptionData }) => {
-        setMessage(messages.concat(subscriptionData.data.messageAdded))
-     }
-  });
-  const [addMessage] = useMutation(addMessageMutation);
-
-  const handleSend = async (text) => {
-    await addMessage({ variables: { input: { text } } });
-  };
+    const {messages, addMessage} = useChatMessage();
 
   return (
       <section className="section">
         <div className="container">
           <h1 className="title">Chatting as {user}</h1>
           <MessageList user={user} messages={messages} />
-          <MessageInput onSend={ handleSend } />
+          <MessageInput onSend={ addMessage } />
         </div>
       </section>
   );
---
+++ client/src/hooks.js
@@ -0,0 +1,21 @@
+import {useMutation, useQuery, useSubscription} from "@apollo/react-hooks";
+import {addMessageMutation, messageAddedSubscription, messagesQuery} from "./graphql/queries";
+
+export const useChatMessage = () => {
+    const { data } = useQuery(messagesQuery);
+    const messages = data ? data.messages : [];
+    useSubscription(messageAddedSubscription, {
+        onSubscriptionData: ({ client, subscriptionData }) => {
+            client.writeData({
+                data: {
+                    messages: messages.concat(subscriptionData.data.messageAdded)
+                }
+            })
+        }
+    });
+    const [addMessage] = useMutation(addMessageMutation);
+    return {
+        messages,
+        addMessage: (text) => addMessage({ variables: { input: { text } } })
+    }
+};
